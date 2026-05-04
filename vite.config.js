f6fc6e1b8f2aaf07835fed1538d8ba412fe45ec1
Weasley18 <harshringsia23cy@rnsit.ac.in>
2026-05-04 15:24:04 +0530

    changes

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
+export default defineConfig(({ command }) => ({
+  base: command === 'build' ? '/portfolio-antigravity/' : '/',
   plugins: [react()],
-})
+}))
 
 
